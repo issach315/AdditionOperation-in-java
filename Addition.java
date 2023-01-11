d46d408218ef03948b18a56cdbd27048d40f0a2e
issach315 <issachemani227@gmial.com>
2023-01-11 11:14:39 +0530

    adding to variables in java

---
+++ Addition.java
@@ -0,0 +1,16 @@
+package Basic_programs;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 12.5;
+
+        double c = a + b;
+
+        System.out.println(c);
+
+        
+
+    }
+
+}
